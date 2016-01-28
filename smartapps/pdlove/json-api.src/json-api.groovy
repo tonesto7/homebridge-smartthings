ee0cf810ebfde699ae1b18091ec16e32b31dc881
pdlove <pdlovelace@gmail.com>
2016-01-27 21:20:23 -0600

    Remove OAUTH
    I can't figure out how to make this be used automatically with OAUTH, so it's being removed here.

--- smartapps/pdlove/json-api.src/json-api.groovy
+++ smartapps/pdlove/json-api.src/json-api.groovy
@@ -21,8 +21,7 @@ definition(
     category: "SmartThings Labs",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    oauth: true)
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 def installed() {
