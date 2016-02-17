fba18cd972cccdd22024e4c976960292a25a2783
pdlove <pdlovelace@gmail.com>
2016-02-17 17:18:02 -0600

    Added acknowledgement of capabilities to prevent false unknown. #42

--- index.js
+++ index.js
@@ -92,7 +92,9 @@ SmartThingsPlatform.prototype = {
 		this.knownCapabilities = ["Switch","Color Control","Battery","Polling","Lock","Refresh","Lock Codes","Sensor","Actuator",
 									"Configuration","Switch Level","Temperature Measurement","Motion Sensor","Color Temperature",
 									"Contact Sensor","Three Axis","Acceleration Sensor","Momentary","Door Control","Garage Door Control",
-									"Relative Humidity Measurement","Presence Sensor","Thermostat", "Energy Meter", "Power Meter"];
+									"Relative Humidity Measurement","Presence Sensor","Thermostat", "Energy Meter", "Power Meter",
+                                    "Thermostat Cooling Setpoint","Thermostat Mode","Thermostat Fan Mode","Thermostat Operating State",
+                                    "Thermostat Heating Setpoint","Thermostat Setpoint","Indicator"];
 		this.temperature_unit = 'F';
 		
 		smartthings.init(this.app_url, this.app_id, this.access_token);
