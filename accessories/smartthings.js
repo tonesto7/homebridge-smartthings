37b157406710eefa9cac1f1294ae3216c6998059
Nick Berardi <nberardi@gmail.com>
2017-06-03 23:44:57 -0700

    Broke out capability for Switch Level between Light and Switch
    Was running into an odd issue with my dimmer switch that it was getting classified as a light bulb.

--- accessories/smartthings.js
+++ accessories/smartthings.js
@@ -87,7 +87,7 @@ function SmartThingsAccessory(platform, device) {
             	    	that.platform.api.runCommand(callback, that.deviceid, "setLevel", {value1: value }); });
 			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
         
-        } else {
+        } else if (device.capabilities["Light"] !== undefined) {
             this.deviceGroup = "lights";
             thisCharacteristic = this.getaddService(Service.Lightbulb).getCharacteristic(Characteristic.On)
             thisCharacteristic.on('get', function(callback) { callback(null, that.device.attributes.switch == "on"); });
@@ -232,6 +232,13 @@ function SmartThingsAccessory(platform, device) {
                     that.platform.api.runCommand(callback, that.deviceid, "off");
             });
 		that.platform.addAttributeUsage("switch", this.deviceid, thisCharacteristic);
+	    
+        if (device.capabilities["Switch Level"] !== undefined) {
+            thisCharacteristic = this.getaddService(Service.Lightbulb).getCharacteristic(Characteristic.Brightness)
+            thisCharacteristic.on('get', function(callback) { callback(null, parseInt(that.device.attributes.level)); });
+            thisCharacteristic.on('set', function(value, callback) { that.platform.api.runCommand(callback, that.deviceid, "setLevel", { value1: value }); });
+			that.platform.addAttributeUsage("level", this.deviceid, thisCharacteristic);
+	}
     }
 
     if ((device.capabilities["Smoke Detector"] !== undefined) && (that.device.attributes.smoke)) {
